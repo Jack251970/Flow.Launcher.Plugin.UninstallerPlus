20ffc89d1cc5efefaadb116ab97c29db09bdd8c4
Jack251970 <1160210343@qq.com>
2025-03-02 19:11:33 +0800

    Initialized main & test project

---
+++ src/BulkCrapUninstaller.WPF.Test/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace BulkCrapUninstaller.WPF.Test;
+
+public partial class App : Application
+{
+
+}
---
+++ src/BulkCrapUninstaller.WPF.Test/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Klocman.Forms.Tools;
+using System.Windows;
+using UninstallTools;
+using UninstallTools.Factory;
+
+namespace BulkCrapUninstaller.WPF.Test;
+
+public partial class MainWindow : Window
+{
+    private IList<ApplicationUninstallerEntry>? _allUninstallers;
+    public IList<ApplicationUninstallerEntry> AllUninstallers
+    {
+        get => _allUninstallers ?? new List<ApplicationUninstallerEntry>();
+        private set
+        {
+            _allUninstallers = value;
+        }
+    }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+        Task.Run(() => ListRefreshThread());
+    }
+
+    private async Task ListRefreshThread(CancellationToken token = default)
+    {
+        await Application.Current.Dispatcher.InvokeAsync(() =>
+        {
+            ProgressBar.Visibility = Visibility.Visible;
+            TextBlock.Visibility = Visibility.Visible;
+            TextBlock.Text = string.Empty;
+            SubProgressBar.Visibility = Visibility.Visible;
+            SubTextBlock.Visibility = Visibility.Visible;
+            SubTextBlock.Text = string.Empty;
+        });
+
+        var progressMax = 0;
+        var uninstallerEntries = ApplicationUninstallerFactory.GetUninstallerEntries(async (x) =>
+        {
+            progressMax = x.TotalCount + 1;
+
+            await Application.Current.Dispatcher.InvokeAsync(() =>
+            {
+                ProgressBar.Maximum = progressMax;
+                ProgressBar.Value = x.CurrentCount;
+                TextBlock.Text = x.Message;
+
+                var inner = x.Inner;
+                if (inner != null)
+                {
+                    SubProgressBar.Maximum = progressMax;
+                    SubProgressBar.Value = x.CurrentCount;
+                    SubTextBlock.Text = inner.Message;
+                }
+                else
+                {
+                    SubProgressBar.Maximum = -1;
+                    SubProgressBar.Value = 0;
+                    SubTextBlock.Text = string.Empty;
+                }
+            });
+
+            token.ThrowIfCancellationRequested();
+        });
+
+        await Application.Current.Dispatcher.InvokeAsync(() =>
+        {
+            ProgressBar.Maximum = progressMax;
+            TextBlock.Text = "Progress finished";
+            SubProgressBar.Maximum = 2;
+            SubProgressBar.Value = 0;
+            SubTextBlock.Text = string.Empty;
+        });
+
+        //if (!string.IsNullOrEmpty(Program.InstalledRegistryKeyName))
+        //    uninstallerEntries.RemoveAll(
+        //        x => PathTools.PathsEqual(x.RegistryKeyName, Program.InstalledRegistryKeyName));
+
+        AllUninstallers = uninstallerEntries;
+
+        /*dialogInterface.SetSubProgress(1, Localisable.Progress_Finishing_Icons);
+        try
+        {
+            _iconGetter.UpdateIconList(AllUninstallers);
+        }
+        catch (Exception ex)
+        {
+            PremadeDialogs.GenericError(ex);
+        }
+
+        dialogInterface.SetSubProgressVisible(false);*/
+    }
+}
