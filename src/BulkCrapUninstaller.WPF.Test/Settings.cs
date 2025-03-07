37bcd6be38323b6bbfe94c7bb455dbc918242207
Jack251970 <1160210343@qq.com>
2025-03-07 19:36:29 +0800

    Add documents for settings

--- src/BulkCrapUninstaller.WPF.Test/Settings.cs
+++ src/BulkCrapUninstaller.WPF.Test/Settings.cs
@@ -2,19 +2,43 @@
 
 public class Settings
 {
+    /// <summary>
+    /// Show applications published by Microsoft
+    /// </summary>
     public bool FilterShowMicrosoft { get; set; } = true;
 
+    /// <summary>
+    /// Show unregistered applications
+    /// </summary>
     public bool AdvancedDisplayOrphans { get; set; } = true;
 
+    /// <summary>
+    /// Show system components
+    /// </summary>
     public bool FilterShowSystemComponents { get; set; } = false;
 
+    /// <summary>
+    /// Show protected items
+    /// </summary>
     public bool FilterShowProtected { get; set; } = false;
 
+    /// <summary>
+    /// Show tweaks
+    /// </summary>
     public bool FilterShowTweaks { get; set; } = true;
 
+    /// <summary>
+    /// Show updates
+    /// </summary>
     public bool FilterShowUpdates { get; set; } = false;
 
+    /// <summary>
+    /// Show Windows features
+    /// </summary>
     public bool FilterShowWinFeatures { get; set; } = false;
 
+    /// <summary>
+    /// Show Windows Store apps
+    /// </summary>
     public bool FilterShowStoreApps { get; set; } = true;
 }
