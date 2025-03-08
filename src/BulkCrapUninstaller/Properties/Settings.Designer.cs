defc50e9e5efe7947480ee14a0d6987329b89868
Jack251970 <1160210343@qq.com>
2025-03-08 02:39:42 -0300

    Fix setting binder issue

--- src/BulkCrapUninstaller/Properties/Settings.Designer.cs
+++ src/BulkCrapUninstaller/Properties/Settings.Designer.cs
@@ -62,29 +62,208 @@ public sealed partial class Settings : ApplicationSettingsBase
     public YesNoAsk MessagesRestorePoints { get; set; } = YesNoAsk.Ask;
 
     // For Functions/AppUninstaller.cs
-    public bool AdvancedDisableProtection { get; set; } = false;
-    public bool CreateRestorePoint { get; set; } = true;
     public bool ExternalEnable { get; set; } = false;
     public string ExternalPreCommands { get; set; } = string.Empty;
-    public bool UninstallConcurrentOneLoud { get; set; } = true;
-    public bool UninstallConcurrency { get; set; } = true;
-    public int UninstallConcurrentMaxCount { get; set; } = 0;
     public string ExternalPostCommands { get; set; } = string.Empty;
     public bool MessagesShowAllBadJunk { get; set; } = false;
-    public bool AdvancedSimulate { get; set; } = false;
-    public bool QuietAutoKillStuck { get; set; } = true;
-    public bool QuietRetryFailedOnce { get; set; } = true;
-
-    // For Forms/Wizards/BeginUninstallTaskWizard.cs
-    public bool AdvancedIntelligentUninstallerSorting { get; set; } = true;
 
     // For Forms/Windows/JunkRemoveWindow.cs
     public string BackupLeftoversDirectory { get; set; } = string.Empty;
 
     // For Controls/Settings/UninstallationSettings.cs
-    public bool UninstallPreventShutdown { get; set; } = true;
-    public bool UninstallConcurrentDisableManualCollisionProtection { get; set; } = false;
-    public bool QuietAutomatization { get; set; } = true;
-    public bool QuietAutomatizationKillStuck { get; set; } = true;
-    public bool QuietUseDaemon { get; set; } = true;
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool UninstallPreventShutdown
+    {
+        get
+        {
+            return ((bool)(this["UninstallPreventShutdown"]));
+        }
+        set
+        {
+            this["UninstallPreventShutdown"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool CreateRestorePoint
+    {
+        get
+        {
+            return ((bool)(this["CreateRestorePoint"]));
+        }
+        set
+        {
+            this["CreateRestorePoint"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool UninstallConcurrency
+    {
+        get
+        {
+            return ((bool)(this["UninstallConcurrency"]));
+        }
+        set
+        {
+            this["UninstallConcurrency"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool UninstallConcurrentOneLoud
+    {
+        get
+        {
+            return ((bool)(this["UninstallConcurrentOneLoud"]));
+        }
+        set
+        {
+            this["UninstallConcurrentOneLoud"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("False")]
+    public bool UninstallConcurrentDisableManualCollisionProtection
+    {
+        get
+        {
+            return ((bool)(this["UninstallConcurrentDisableManualCollisionProtection"]));
+        }
+        set
+        {
+            this["UninstallConcurrentDisableManualCollisionProtection"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("2")]
+    public int UninstallConcurrentMaxCount
+    {
+        get
+        {
+            return ((int)(this["UninstallConcurrentMaxCount"]));
+        }
+        set
+        {
+            this["UninstallConcurrentMaxCount"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool AdvancedIntelligentUninstallerSorting
+    {
+        get
+        {
+            return ((bool)(this["AdvancedIntelligentUninstallerSorting"]));
+        }
+        set
+        {
+            this["AdvancedIntelligentUninstallerSorting"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("False")]
+    public bool AdvancedDisableProtection
+    {
+        get
+        {
+            return ((bool)(this["AdvancedDisableProtection"]));
+        }
+        set
+        {
+            this["AdvancedDisableProtection"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("False")]
+    public bool AdvancedSimulate
+    {
+        get
+        {
+            return ((bool)(this["AdvancedSimulate"]));
+        }
+        set
+        {
+            this["AdvancedSimulate"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool QuietAutoKillStuck
+    {
+        get
+        {
+            return ((bool)(this["QuietAutoKillStuck"]));
+        }
+        set
+        {
+            this["QuietAutoKillStuck"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool QuietRetryFailedOnce
+    {
+        get
+        {
+            return ((bool)(this["QuietRetryFailedOnce"]));
+        }
+        set
+        {
+            this["QuietRetryFailedOnce"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool QuietAutomatization
+    {
+        get
+        {
+            return ((bool)(this["QuietAutomatization"]));
+        }
+        set
+        {
+            this["QuietAutomatization"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool QuietAutomatizationKillStuck
+    {
+        get
+        {
+            return ((bool)(this["QuietAutomatizationKillStuck"]));
+        }
+        set
+        {
+            this["QuietAutomatizationKillStuck"] = value;
+        }
+    }
+
+    [global::System.Configuration.UserScopedSettingAttribute()]
+    [global::System.Configuration.DefaultSettingValueAttribute("True")]
+    public bool QuietUseDaemon
+    {
+        get
+        {
+            return ((bool)(this["QuietUseDaemon"]));
+        }
+        set
+        {
+            this["QuietUseDaemon"] = value;
+        }
+    }
 }
