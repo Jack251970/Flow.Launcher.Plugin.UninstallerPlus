fe86e099da15f0ccc901dab7164ee837a934d63b
Jack251970 <1160210343@qq.com>
2025-05-16 00:21:12 +0800

    Use BaseModel for settings

--- src/Flow.Launcher.Plugin.UninstallerPlus/Settings.cs
+++ src/Flow.Launcher.Plugin.UninstallerPlus/Settings.cs
@@ -4,49 +4,145 @@ using System.Text.Json.Serialization;
 
 namespace Flow.Launcher.Plugin.UninstallerPlus;
 
-public class Settings
+public class Settings : BaseModel
 {
     #region Filter Settings
 
     /// <summary>
     /// Show applications published by Microsoft
     /// </summary>
-    public bool FilterShowMicrosoft { get; set; } = true;
+    private bool _filterShowMicrosoft = true;
+    public bool FilterShowMicrosoft
+    {
+        get => _filterShowMicrosoft;
+        set
+        {
+            if (_filterShowMicrosoft != value)
+            {
+                _filterShowMicrosoft = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     /// <summary>
     /// Show unregistered applications
     /// </summary>
-    public bool AdvancedDisplayOrphans { get; set; } = true;
+    private bool _advancedDisplayOrphans = true;
+    public bool AdvancedDisplayOrphans
+    {
+        get => _advancedDisplayOrphans;
+        set
+        {
+            if (_advancedDisplayOrphans != value)
+            {
+                _advancedDisplayOrphans = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     /// <summary>
     /// Show system components
     /// </summary>
-    public bool FilterShowSystemComponents { get; set; } = false;
+    private bool _filterShowSystemComponents = false;
+    public bool FilterShowSystemComponents
+    {
+        get => _filterShowSystemComponents;
+        set
+        {
+            if (_filterShowSystemComponents != value)
+            {
+                _filterShowSystemComponents = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     /// <summary>
     /// Show protected items
     /// </summary>
-    public bool FilterShowProtected { get; set; } = false;
+    private bool _filterShowProtected = false;
+    public bool FilterShowProtected
+    {
+        get => _filterShowProtected;
+        set
+        {
+            if (_filterShowProtected != value)
+            {
+                _filterShowProtected = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     /// <summary>
     /// Show tweaks
     /// </summary>
-    public bool FilterShowTweaks { get; set; } = true;
+    private bool _filterShowTweaks = true;
+    public bool FilterShowTweaks
+    {
+        get => _filterShowTweaks;
+        set
+        {
+            if (_filterShowTweaks != value)
+            {
+                _filterShowTweaks = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     /// <summary>
     /// Show updates
     /// </summary>
-    public bool FilterShowUpdates { get; set; } = false;
+    private bool _filterShowUpdates = false;
+    public bool FilterShowUpdates
+    {
+        get => _filterShowUpdates;
+        set
+        {
+            if (_filterShowUpdates != value)
+            {
+                _filterShowUpdates = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     /// <summary>
     /// Show Windows features
     /// </summary>
-    public bool FilterShowWinFeatures { get; set; } = false;
+    private bool _filterShowWinFeatures = false;
+    public bool FilterShowWinFeatures
+    {
+        get => _filterShowWinFeatures;
+        set
+        {
+            if (_filterShowWinFeatures != value)
+            {
+                _filterShowWinFeatures = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     /// <summary>
     /// Show Windows Store apps
     /// </summary>
-    public bool FilterShowStoreApps { get; set; } = true;
+    private bool _filterShowStoreApps = true;
+    public bool FilterShowStoreApps
+    {
+        get => _filterShowStoreApps;
+        set
+        {
+            if (_filterShowStoreApps != value)
+            {
+                _filterShowStoreApps = value;
+                OnPropertyChanged();
+            }
+        }
+    }
 
     #endregion
 
